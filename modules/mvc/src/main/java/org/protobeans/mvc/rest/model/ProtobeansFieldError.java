ca0f4f39136613141191a067e10f3511ac074892
П Вова <man4j@ya.ru>
2018-07-25 16:48:56 +0300

    fix

--- modules/mvc/src/main/java/org/protobeans/mvc/rest/model/ProtobeansFieldError.java
+++ modules/mvc/src/main/java/org/protobeans/mvc/rest/model/ProtobeansFieldError.java
@@ -1,11 +1,15 @@
 package org.protobeans.mvc.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ProtobeansFieldError {
     private String fieldName;
     
     private String fieldError;
 
-    public ProtobeansFieldError(String fieldName, String fieldError) {
+    @JsonCreator
+    public ProtobeansFieldError(@JsonProperty("fieldName") String fieldName, @JsonProperty("fieldError") String fieldError) {
         this.fieldName = fieldName;
         this.fieldError = fieldError;
     }
