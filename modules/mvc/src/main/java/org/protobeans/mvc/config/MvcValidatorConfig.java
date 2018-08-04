636b86349fd22a1dab30eb8f0fd2fd2defc88294
П Вова < >
2018-08-04 10:36:36 +0300

    fix

--- modules/mvc/src/main/java/org/protobeans/mvc/config/MvcValidatorConfig.java
+++ modules/mvc/src/main/java/org/protobeans/mvc/config/MvcValidatorConfig.java
@@ -1,15 +1,10 @@
 package org.protobeans.mvc.config;
 
-import java.lang.reflect.Field;
-
 import org.protobeans.mvc.util.ProtobeansMessageInterpolator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.DelegatingMessageSource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MessageSourceResourceBundleLocator;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
@@ -20,9 +15,6 @@ import org.springframework.validation.beanvalidation.MethodValidationPostProcess
  */
 @Configuration
 public class MvcValidatorConfig {
-    @Autowired
-    private MessageSource messageSource;
-
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         MethodValidationPostProcessor methodValidationPostProcessor = new MethodValidationPostProcessor();
@@ -33,39 +25,31 @@ public class MvcValidatorConfig {
     }
     
     @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasenames("classpath:messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(isCached() ? -1 : 0);
+        messageSource.setFallbackToSystemLocale(false);
+
+        return messageSource;
+    }
+
+    private boolean isCached() {
+        boolean isCached = true;
+        
+        if (System.getProperty("os.name").toLowerCase().contains("windows") || System.getProperty("cacheResources").equalsIgnoreCase("false")) {
+            isCached = false;
+        }
+        return isCached;
+    }
+    
+    @Bean
     public LocalValidatorFactoryBean localValidatorFactoryBean() {
         LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
         
-        boolean cached = true;
-        
-        if (messageSource instanceof DelegatingMessageSource) {
-            DelegatingMessageSource dms = (DelegatingMessageSource) messageSource;
-
-            MessageSource parentMessageSource = dms.getParentMessageSource();
-
-            while (parentMessageSource instanceof DelegatingMessageSource) {
-                parentMessageSource = ((DelegatingMessageSource) parentMessageSource).getParentMessageSource();
-            }
-            
-            if (parentMessageSource instanceof ReloadableResourceBundleMessageSource) {
-                ReloadableResourceBundleMessageSource rms = (ReloadableResourceBundleMessageSource) parentMessageSource;
-
-                Field f = ReflectionUtils.findField(ReloadableResourceBundleMessageSource.class, "cacheMillis");
-                
-                f.setAccessible(true);
-                
-                try {
-                    long millis = f.getLong(rms);
-                    
-                    cached = (millis == -1);
-                } catch (IllegalArgumentException | IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-        
-        //validatorFactoryBean.setValidationMessageSource(messageSource);
-        validatorFactoryBean.setMessageInterpolator(new ProtobeansMessageInterpolator(new MessageSourceResourceBundleLocator(messageSource), cached));
+        validatorFactoryBean.setMessageInterpolator(new ProtobeansMessageInterpolator(new MessageSourceResourceBundleLocator(messageSource()), isCached()));
         
         return validatorFactoryBean;
     }
