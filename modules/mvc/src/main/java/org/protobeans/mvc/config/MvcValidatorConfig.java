6389396d93a72158ea1f0589baa7f3969dccd22e
П Вова < >
2018-08-24 16:08:42 +0300

    new version

--- modules/mvc/src/main/java/org/protobeans/mvc/config/MvcValidatorConfig.java
+++ modules/mvc/src/main/java/org/protobeans/mvc/config/MvcValidatorConfig.java
@@ -4,6 +4,7 @@ import org.protobeans.mvc.util.ProtobeansMessageInterpolator;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MessageSourceResourceBundleLocator;
@@ -47,6 +48,7 @@ public class MvcValidatorConfig {
     }
     
     @Bean
+    @Primary
     public LocalValidatorFactoryBean localValidatorFactoryBean() {
         LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
         
