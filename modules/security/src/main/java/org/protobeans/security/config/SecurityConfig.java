2f746a87d607910d4a87c29c786e479e36ea839d
man4j <man4j@ya.ru>
2019-10-28 14:04:15 +0300

    update couchdb

--- modules/security/src/main/java/org/protobeans/security/config/SecurityConfig.java
+++ modules/security/src/main/java/org/protobeans/security/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         
         http.authenticationProvider(new UuidAuthenticationProvider())//add custom provider
             .authorizeRequests().mvcMatchers(permitAllPatterns).permitAll()
-                                .antMatchers("/swagger-ui.html/**", "/swagger-resources/**", "/v2/api-docs/**", "/webjars/**").permitAll()
+                                .antMatchers("/swagger-ui.html/**", "/swagger-resources/**", "/v2/api-docs/**", "/webjars/**", "/csrf").permitAll()
                                 .mvcMatchers(anonymousPatterns).anonymous()
                                 .anyRequest().authenticated()            
             .and().rememberMe().rememberMeServices(rememberMeServices()).key("123").authenticationSuccessHandler(new CurrentUrlAuthenticationSuccessHandler())
