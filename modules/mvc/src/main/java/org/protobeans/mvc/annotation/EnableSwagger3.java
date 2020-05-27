fd2d08e1bac8266fec7c7df5401200c8f1fe186b
man4j <man4j@ya.ru>
2020-05-27 12:06:05 +0300

    fix swagger

--- modules/mvc/src/main/java/org/protobeans/mvc/annotation/EnableSwagger3.java
+++ modules/mvc/src/main/java/org/protobeans/mvc/annotation/EnableSwagger3.java
@@ -5,24 +5,33 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.protobeans.mvc.config.Swagger3Config;
-import org.springdoc.core.MultipleOpenApiSupportConfiguration;
 import org.springdoc.core.SpringDocConfigProperties;
 import org.springdoc.core.SpringDocConfiguration;
-import org.springdoc.core.SpringDocWebMvcConfiguration;
 import org.springdoc.core.SwaggerUiConfigProperties;
 import org.springdoc.core.SwaggerUiOAuthProperties;
+import org.springdoc.webmvc.core.MultipleOpenApiSupportConfiguration;
+import org.springdoc.webmvc.core.SpringDocWebMvcConfiguration;
+import org.springdoc.webmvc.ui.SwaggerConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({Swagger3Config.class,
-         MultipleOpenApiSupportConfiguration.class,
+@Import({SpringDocConfiguration.class,
          SpringDocWebMvcConfiguration.class,
-         SpringDocConfiguration.class,
+         
          SpringDocConfigProperties.class,
          SwaggerUiConfigProperties.class,
-         SwaggerUiOAuthProperties.class})
+         SwaggerUiOAuthProperties.class,
+         
+         MultipleOpenApiSupportConfiguration.class,
+         SwaggerConfig.class})
+@Configuration
+@EnableConfigurationProperties
+@PropertySource("classpath:swagger.properties")
 public @interface EnableSwagger3 {
+    
 
 }
