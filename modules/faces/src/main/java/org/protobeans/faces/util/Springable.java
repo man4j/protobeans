bd635cb42d78ad1ea8bd9f533146bd63c85da1f3
mylonglongname <man4j@ya.ru>
2023-02-01 19:56:51 +0300

    fixes

--- modules/faces/src/main/java/org/protobeans/faces/util/Springable.java
+++ modules/faces/src/main/java/org/protobeans/faces/util/Springable.java
@@ -2,11 +2,12 @@ package org.protobeans.faces.util;
 
 import java.io.Serializable;
 
+import org.protobeans.faces.config.ProtobeansFacesConfig;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 
 public class Springable implements Serializable {    
     public Springable() {
-	    AutowireCapableBeanFactory ctx = WebApplicationContextUtils.getApplicationContext().getAutowireCapableBeanFactory();
+	    AutowireCapableBeanFactory ctx = ProtobeansFacesConfig.springContext.getAutowireCapableBeanFactory();
 	    
 	    ctx.autowireBean(this);
 	}
