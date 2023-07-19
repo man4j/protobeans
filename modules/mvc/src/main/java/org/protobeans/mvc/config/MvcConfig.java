a63cbc34b518f569698145c260cd4d027c1dd6b2
Вова <man4j@ya.ru>
2023-07-19 12:12:12 +0300

    new version

--- modules/mvc/src/main/java/org/protobeans/mvc/config/MvcConfig.java
+++ modules/mvc/src/main/java/org/protobeans/mvc/config/MvcConfig.java
@@ -44,6 +44,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 
 @EnableWebMvc
@@ -88,6 +89,7 @@ public class MvcConfig implements WebMvcConfigurer {
                                    .serializationInclusion(Include.NON_NULL)
                                    .build()
                                    .registerModule(new JavaTimeModule())
+                                   .registerModule(new ParameterNamesModule())
                                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
     
@@ -158,4 +160,3 @@ public class MvcConfig implements WebMvcConfigurer {
         return new StandardServletMultipartResolver();
     }
 }
-
