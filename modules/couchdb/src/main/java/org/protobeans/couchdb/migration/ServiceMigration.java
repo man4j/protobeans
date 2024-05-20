6cfe12099ae2b2ca95d8048223374964abb54796
Владимир <man4j@ya.ru>
2024-05-20 17:06:54 +0300

    new version

--- modules/couchdb/src/main/java/org/protobeans/couchdb/migration/ServiceMigration.java
+++
@@ -1,7 +0,0 @@
-package org.protobeans.couchdb.migration;
-
-public interface ServiceMigration {
-    int getVersion();
-    
-    void migrate();
-}
--- modules/couchdb/src/main/java/org/protobeans/couchdb/model/CouchDbMigration.java
+++
@@ -1,7 +0,0 @@
-package org.protobeans.couchdb.model;
-
-public interface CouchDbMigration {
-    int getVersion();
-    
-    void migrate();
-}
