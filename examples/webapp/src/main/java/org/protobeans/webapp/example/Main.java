f4568f67d96712bdc20ba9d8585b3f4f9671df4d
Вова <man4j@ya.ru>
2024-07-02 14:54:42 +0300

    update version

--- examples/webapp/src/main/java/org/protobeans/webapp/example/Main.java
+++ examples/webapp/src/main/java/org/protobeans/webapp/example/Main.java
@@ -18,11 +18,11 @@ import org.springframework.context.annotation.PropertySource;
 @EnableSecurity
 @EnableFeign
 @EnableMail(host = "smtp.sendgrid.net", user = "apikey", password = "${emailPassword}")
-@EnablePostgreSql(showSql = "false", schema = "demo", dbHost = "127.0.0.1", dbPort = "5432", user = "postgres", password = "PassWord111", basePackages = {"org.protobeans.webapp.example"}, migrationsPath = "migrations")
+@EnablePostgreSql(showSql = "false", schema = "demo", dbHost = "127.0.0.1", dbPort = "5432", user = "postgres", password = "changeme", basePackages = {"org.protobeans.webapp.example"}, migrationsPath = "migrations")
 @ComponentScan(basePackages = "org.protobeans.webapp.example")
 @EnableSwagger3
 @PropertySource("classpath:application.properties")
-public class Main {
+public class Main {    
     @SuppressWarnings("resource")
     public static void main(String[] args) {
         MvcEntryPoint.run(Main.class);
--- examples/webapp/src/main/java/org/protobeans/webapp/example/controller/AdminController.java
+++ examples/webapp/src/main/java/org/protobeans/webapp/example/controller/AdminController.java
@@ -4,9 +4,17 @@ import org.protobeans.security.annotation.AdminRole;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.annotation.PostConstruct;
+
 @Controller
 @AdminRole
 public class AdminController {
+    
+    @PostConstruct
+    void init() {
+        System.out.println("123");
+    }
+    
     @GetMapping(path = "/admin")
     String getResult() {
         return "/admin";
